92c8b14a591d970f0f576df5ad3371e13af83221
Tomas Polivka <tom.polivka96@gmail.com>
2022-09-11 23:31:50 +0200

    hidden db secrets

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,12 @@
 import { connect } from 'mongoose';
 
+const uri = process.env.MONGODB_URI ?? '';
+
 export const connectToDb = async () => {
     try {
-        connect(
-            'mongodb+srv://tpolivka1123581321:Trolol321@cipherblitzcluster.wbap4r3.mongodb.net/?retryWrites=true&w=majority',
-            () => {
-                console.log('connected to db');
-            }
-        );
+        connect(uri, () => {
+            console.log('connected to db');
+        });
     } catch (error) {
         console.log(error);
     }
